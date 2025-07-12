353a8395fb34fe4ddd40d545436df54f82f443d9
happyhzr <insisthzr@163.com>
2025-07-12 21:03:12 +0800

    add github oauth

---
+++ components/Navbar.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link"
+import Image from "next/image"
+
+import { auth, signOut, signIn } from "@/auth"
+
+export default async function Navbar() {
+    const session = await auth()
+
+    return (
+        <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex items-center justify-between">
+                <Link href='/'>
+                    <Image src='/panda.webp' alt="logo" width={144} height={30} />
+                </Link>
+                <div className="flex items-center gap-5">
+                    {
+                        session && session?.user ? (
+                            <div>
+                                <Link href='/startup/create'>
+                                    <span>Create</span>
+                                </Link>
+                                <button onClick={
+                                    async () => {
+                                        "use server"
+                                        await signOut()
+                                    }
+                                }>
+                                    <span>Logout</span>
+                                </button>
+                                <Link href={`/user/${session?.user?.id}`}>
+                                    <span>{session?.user?.name}</span>
+                                </Link>
+                            </div>
+                        ) : (
+                            <button onClick={
+                                async () => {
+                                    "use server"
+                                    await signIn("github", { redirectTo: "/" })
+                                }
+                            }>
+                                <span>Login</span>
+                            </button>
+                        )
+                    }
+                </div>
+            </nav>
+        </div>
+    )
+}
